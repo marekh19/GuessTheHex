bcde2a94d39a09a57759d2e0bd5293e2cc7c2d17
Marek Honzal <marek.honzal@gmail.com>
2022-10-21 23:36:06 +0200

    chore: setup of colors, fonts etc

---
+++ src/features/ui/theme/colors.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  background: {
+    base: '#222831',
+    lighter: '#393E46',
+  },
+  accent: {
+    base: '#00ADB5',
+  },
+  text: {
+    base: '#EEEEEE',
+  },
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import type { AppProps } from 'next/app'
 
+import { GlobalStyle } from '../features/ui/theme/global'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
