e849eec0bc1ac930edeabb5265e9db810dd2ff55
Marek Honzal <marek.honzal@gmail.com>
2022-11-02 21:03:10 +0100

    feat: getting random hex works with useEffect onload

--- src/features/home/parts/ColorFrame/index.tsx
+++ src/features/home/parts/ColorFrame/index.tsx
@@ -1,13 +1,17 @@
 import type { FC } from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
 import { ColorFrameWrapper } from './styled'
 
-// const getRandomHex = (): string => {
-//   return '#' + Math.floor(Math.random() * 16777215).toString(16)
-// }
+const getRandomHex = (): string => {
+  return `#${((Math.random() * 0xffffff) << 0).toString(16).padStart(6, '0')}`
+}
 
 export const ColorFrame: FC = () => {
-  // const [color, setColor] = useState(getRandomHex())
-  // return <ColorFrameWrapper color={color} />
-  return <ColorFrameWrapper color="red" />
+  const [color, setColor] = useState('')
+  useEffect(() => {
+    setColor(getRandomHex())
+  }, [])
+  return <ColorFrameWrapper color={color} />
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,28 +1,52 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import type { DocumentContext, DocumentInitialProps } from 'next/document'
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin=""
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
+type TDocument = {
+  (): JSX.Element
+  getInitialProps: (ctx: DocumentContext) => Promise<DocumentInitialProps>
+}
+
+const Document: TDocument = () => (
+  <Html lang="en">
+    <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
+        rel="stylesheet"
+      />
+    </Head>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+)
+
+Document.getInitialProps = async (ctx) => {
+  const sheet = new ServerStyleSheet()
+  const originalRenderPage = ctx.renderPage
+
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      })
+
+    const initialProps = await NextDocument.getInitialProps(ctx)
+    return {
+      ...initialProps,
+      styles: [
+        <>
+          {initialProps.styles}
+          {sheet.getStyleElement()}
+        </>,
+      ],
+    }
+  } finally {
+    sheet.seal()
   }
 }
 
-export default MyDocument
+export default Document
